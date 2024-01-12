d8739adbdc29cef3a30dcb400fa0ef6459a3116b
Akanbi Gisele <akanbi@planetkoncept.pro>
2024-01-12 14:00:18 +0000

    modification des logo media sociaux

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,3 +1,5 @@
+<!doctype html>
+<html class="no-js"  lang="en">
 <head>
           @include('partial.head')
     </head>
@@ -105,5 +107,241 @@
 
     <center><h2>Contact</h2></center>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A dolore inventore provident, suscipit, blanditiis hic fugit perspiciatis ab in quaerat sed laudantium necessitatibus officiis, aspernatur reiciendis odit doloremque natus quos.</p>
-@include('partial.footer')
-@include('partial.script')
+
+    
+<section   class="testemonial">
+    <div class="container">
+
+        <div class="gallary-header text-center">
+            <h2>
+                clients reviews
+            </h2>
+            <p>
+                Duis aute irure dolor in  velit esse cillum dolore eu fugiat nulla. 
+            </p>
+
+        </div><!--/.gallery-header-->
+
+        <div class="owl-carousel owl-theme" id="testemonial-carousel">
+
+            <div class="home1-testm item">
+                <div class="home1-testm-single text-center">
+                    <div class="home1-testm-img">
+                        <img src="{{asset('admin/assets/images/client/testimonial1.jpg')}}" alt="img"/>
+                    </div><!--/.home1-testm-img-->
+                    <div class="home1-testm-txt">
+                        <span class="icon section-icon">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                        </span>
+                        <p>
+                            Lorem ipsum dolor sit amet, contur adip elit, sed do mod incid ut labore et dolore magna aliqua. Ut enim ad minim veniam. 
+                        </p>
+                        <h3>
+                            <a href="#">
+                                Ouattara Harouna
+                            </a>
+                        </h3>
+                        <h4>Korhogo, Côte d'ivoire</h4>
+                    </div><!--/.home1-testm-txt-->	
+                </div><!--/.home1-testm-single-->
+
+            </div><!--/.item-->
+
+            <div class="home1-testm item">
+                <div class="home1-testm-single text-center">
+                    <div class="home1-testm-img">
+                        <img src="{{asset('admin/assets/images/client/testimonial2.jpg')}}" alt="img"/>
+                    </div><!--/.home1-testm-img-->
+                    <div class="home1-testm-txt">
+                        <span class="icon section-icon">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                        </span>
+                        <p>
+                            Lorem ipsum dolor sit amet, contur adip elit, sed do mod incid ut labore et dolore magna aliqua. Ut enim ad minim veniam. 
+                        </p>
+                        <h3>
+                            <a href="#">
+                                kevin watson
+                            </a>
+                        </h3>
+                        <h4>london, england</h4>
+                    </div><!--/.home1-testm-txt-->	
+                </div><!--/.home1-testm-single-->
+
+            </div><!--/.item-->
+
+            <div class="home1-testm item">
+                <div class="home1-testm-single text-center">
+                    <div class="home1-testm-img">
+                        <img src="{{asset('admin/assets/images/client/testimonial1.jpg')}}" alt="img"/>
+                    </div><!--/.home1-testm-img-->
+                    <div class="home1-testm-txt">
+                        <span class="icon section-icon">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                        </span>
+                        <p>
+                            Lorem ipsum dolor sit amet, contur adip elit, sed do mod incid ut labore et dolore magna aliqua. Ut enim ad minim veniam. 
+                        </p>
+                        <h3>
+                            <a href="#">
+                                kevin watson
+                            </a>
+                        </h3>
+                        <h4>london, england</h4>
+                    </div><!--/.home1-testm-txt-->	
+                </div><!--/.home1-testm-single-->
+
+            </div><!--/.item-->
+
+            <div class="home1-testm item">
+                <div class="home1-testm-single text-center">
+                    <div class="home1-testm-img">
+                        <img src="{{asset('admin/assets/images/client/testimonial1.jpg')}}" alt="img"/>
+                    </div><!--/.home1-testm-img-->
+                    <div class="home1-testm-txt">
+                        <span class="icon section-icon">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                        </span>
+                        <p>
+                            Lorem ipsum dolor sit amet, contur adip elit, sed do mod incid ut labore et dolore magna aliqua. Ut enim ad minim veniam. 
+                        </p>
+                        <h3>
+                            <a href="#">
+                                kevin watson
+                            </a>
+                        </h3>
+                        <h4>london, england</h4>
+                    </div><!--/.home1-testm-txt-->	
+                </div><!--/.home1-testm-single-->
+
+            </div><!--/.item-->
+
+            <div class="home1-testm item">
+                <div class="home1-testm-single text-center">
+                    <div class="home1-testm-img">
+                        <img src="{{asset('admin/assets/images/client/testimonial2.jpg')}}" alt="img"/>
+                    </div><!--/.home1-testm-img-->
+                    <div class="home1-testm-txt">
+                        <span class="icon section-icon">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                        </span>
+                        <p>
+                            Lorem ipsum dolor sit amet, contur adip elit, sed do mod incid ut labore et dolore magna aliqua. Ut enim ad minim veniam. 
+                        </p>
+                        <h3>
+                            <a href="#">
+                                kevin watson
+                            </a>
+                        </h3>
+                        <h4>london, england</h4>
+                    </div><!--/.home1-testm-txt-->	
+                </div><!--/.home1-testm-single-->
+
+            </div><!--/.item-->
+
+            <div class="home1-testm item">
+                <div class="home1-testm-single text-center">
+                    <div class="home1-testm-img">
+                        <img src="{{asset('admin/assets/images/client/testimonial1.jpg')}}" alt="img"/>
+                    </div><!--/.home1-testm-img-->
+                    <div class="home1-testm-txt">
+                        <span class="icon section-icon">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                        </span>
+                        <p>
+                            Lorem ipsum dolor sit amet, contur adip elit, sed do mod incid ut labore et dolore magna aliqua. Ut enim ad minim veniam. 
+                        </p>
+                        <h3>
+                            <a href="#">
+                                kevin watson
+                            </a>
+                        </h3>
+                        <h4>london, england</h4>
+                    </div><!--/.home1-testm-txt-->	
+                </div><!--/.home1-testm-single-->
+
+            </div><!--/.item-->
+
+            <div class="home1-testm item">
+                <div class="home1-testm-single text-center">
+                    <div class="home1-testm-img">
+                        <img src="{{asset('admin/assets/images/client/testimonial1.jpg')}}" alt="img"/>
+                    </div><!--/.home1-testm-img-->
+                    <div class="home1-testm-txt">
+                        <span class="icon section-icon">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                        </span>
+                        <p>
+                            Lorem ipsum dolor sit amet, contur adip elit, sed do mod incid ut labore et dolore magna aliqua. Ut enim ad minim veniam. 
+                        </p>
+                        <h3>
+                            <a href="#">
+                                kevin watson
+                            </a>
+                        </h3>
+                        <h4>london, england</h4>
+                    </div><!--/.home1-testm-txt-->	
+                </div><!--/.home1-testm-single-->
+
+            </div><!--/.item-->
+
+            <div class="home1-testm item">
+                <div class="home1-testm-single text-center">
+                    <div class="home1-testm-img">
+                        <img src="{{asset('admin/assets/images/client/testimonial2.jpg')}}" alt="img"/>
+                    </div><!--/.home1-testm-img-->
+                    <div class="home1-testm-txt">
+                        <span class="icon section-icon">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                        </span>
+                        <p>
+                            Lorem ipsum dolor sit amet, contur adip elit, sed do mod incid ut labore et dolore magna aliqua. Ut enim ad minim veniam. 
+                        </p>
+                        <h3>
+                            <a href="#">
+                                kevin watson
+                            </a>
+                        </h3>
+                        <h4>london, england</h4>
+                    </div><!--/.home1-testm-txt-->	
+                </div><!--/.home1-testm-single-->
+
+            </div><!--/.item-->
+
+            <div class="home1-testm item">
+                <div class="home1-testm-single text-center">
+                    <div class="home1-testm-img">
+                        <img src="{{asset('admin/assets/images/client/testimonial1.jpg')}}" alt="img"/>
+                    </div><!--/.home1-testm-img-->
+                    <div class="home1-testm-txt">
+                        <span class="icon section-icon">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                        </span>
+                        <p>
+                            Lorem ipsum dolor sit amet, contur adip elit, sed do mod incid ut labore et dolore magna aliqua. Ut enim ad minim veniam. 
+                        </p>
+                        <h3>
+                            <a href="#">
+                                kevin watson
+                            </a>
+                        </h3>
+                        <h4>london, england</h4>
+                    </div><!--/.home1-testm-txt-->	
+                </div><!--/.home1-testm-single-->
+
+            </div><!--/.item-->
+
+        </div><!--/.testemonial-carousel-->
+    </div><!--/.container-->
+
+</section><!--/.testimonial-->	
+<!-- testemonial End -->
+	<!-- footer-copyright start -->
+    <footer  class="footer-copyright">
+            @include('partial.footer')
+
+		</footer><!-- /.footer-copyright-->
+		<!-- footer-copyright end -->
+
+        @include('partial.script')
+</html>
