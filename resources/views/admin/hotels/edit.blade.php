956f8d6d541a71091030555e9a86ffa092ec8844
Sidik Slims <sidikslim@outlook.com>
2024-03-06 09:50:31 +0000

    Create Hotel and statistic show

---
+++ resources/views/admin/hotels/edit.blade.php
@@ -0,0 +1,54 @@
+
+@extends('admin.layout.layout')
+@section('content')
+    <div class="container">
+        <h1>Modifier l'Hotel</h1>
+
+        <div class="card">
+            <div class="card-body">
+                <form action="{{ route('hotels.update', $hotel->id) }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+
+                    <!-- Add form fields for editing hotel details -->
+                    <div class="form-group">
+                        <label for="name">Nom:</label>
+                        <input type="text" name="name" id="name" class="form-control" value="{{ $hotel->name }}" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="description">Description:</label>
+                        <textarea name="description" id="description" class="form-control" required>{{ $hotel->description }}</textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="localisation">Localisation:</label>
+                        <input type="text" name="localisation" id="localisation" class="form-control" value="{{ $hotel->localisation }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="etoiles">Étoiles:</label>
+                        <input type="number" name="etoiles" id="etoiles" class="form-control" value="{{ $hotel->etoiles }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="prix">Prix:</label>
+                        <input type="number" name="prix" id="prix" class="form-control" value="{{ $hotel->prix }}" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="images">Images:</label>
+                        <input type="file" name="images[]" id="images" multiple accept="image/*" class="form-control-file">
+                    </div>
+
+                    <!-- Add other form fields for editing -->
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Enregistrer les modifications</button>
+                        <a href="{{ route('hotels.index') }}" class="btn btn-secondary">Annuler</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,97 @@
 @extends('admin.layout.layout')
-    @section('content')
+@section('content')
+<section>
+    <div class="row ">
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+          <div class="card">
+            <div class="card-statistic-4">
+              <div class="align-items-center justify-content-between">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                        <div class="card-content">
+                            <h5 class="font-15">Tours</h5>
+                            <h2 class="mb-3 font-18">{{ $totalTours }}</h2>
+                            <p class="mb-0"><span class="col-green">{{ number_format($percentageIncrease) }}%</span> Augmentation</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                        <div class="banner-img">
+                            <img src="assets/img/banner/2149153260.png" alt="">
+                        </div>
+                    </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+          <div class="card">
+            <div class="card-statistic-4">
+              <div class="align-items-center justify-content-between">
+                <div class="row ">
+                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                    <div class="card-content">
+                        <h5 class="font-15">Hotels</h5>
+                        <h2 class="mb-3 font-18">{{ $totalHotels }}</h2>
+                        <p class="mb-0"><span class="col-green">{{ number_format($percentageIncreaseHotels) }}%</span> Augmentation</p>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                    <div class="banner-img">
+                      <img src="assets/img/banner/8316271.png" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+          <div class="card">
+            <div class="card-statistic-4">
+              <div class="align-items-center justify-content-between">
+                <div class="row ">
+                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                    <div class="card-content">
+                      <h5 class="font-15">New Project</h5>
+                      <h2 class="mb-3 font-18">128</h2>
+                      <p class="mb-0"><span class="col-green">18%</span>
+                        Increase</p>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                    <div class="banner-img">
+                      <img src="assets/img/banner/3.png" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+          <div class="card">
+            <div class="card-statistic-4">
+              <div class="align-items-center justify-content-between">
+                <div class="row ">
+                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                    <div class="card-content">
+                      <h5 class="font-15">Revenue</h5>
+                      <h2 class="mb-3 font-18">$48,697</h2>
+                      <p class="mb-0"><span class="col-green">42%</span> Increase</p>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                    <div class="banner-img">
+                      <img src="assets/img/banner/4.png" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</section>
 @endsection
