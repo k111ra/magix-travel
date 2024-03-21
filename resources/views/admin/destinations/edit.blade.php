0978ef63ebe40087d7ec7a86fac17380136954b1
Akanbi Gisele <akanbi@planetkoncept.pro>
2024-03-21 13:31:35 +0000

    modif des formulaires

--- resources/views/admin/destinations/edit.blade.php
+++ resources/views/admin/destinations/edit.blade.php
@@ -11,26 +11,36 @@
                     @method('PUT')
 
                     <!-- Add form fields for editing destination details -->
-                    <div class="form-group">
-                        <label for="name">Nom:</label>
-                        <input type="text" name="name" id="name" class="form-control" value="{{ $destination->name }}" required>
-                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label for="name">Nom:</label>
+                                <input type="text" name="name" id="name" class="form-control" value="{{ $destination->name }}" required>
+                            </div>
+                        </div>
 
-                    <div class="form-group">
-                        <label for="description">Description:</label>
-                        <textarea name="description" id="description" class="form-control" required>{{ $destination->description }}</textarea>
-                    </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label for="images">Images:</label>
+                                <input type="file" name="images[]" id="images" multiple accept="image/*" class="form-control-file">
+                            </div>
+                        </div>
+                        
 
-                    <div class="form-group">
-                        <label for="images">Images:</label>
-                        <input type="file" name="images[]" id="images" multiple accept="image/*" class="form-control-file">
-                    </div>
+                        <div class="col-sm-12">
+                        <div class="form-group">
+                                <label for="description">Description:</label>
+                                <textarea name="description" id="description" class="form-control" required>{{ $destination->description }}</textarea>
+                            </div>
+                        </div>
 
-                    <!-- Add other form fields for editing -->
+                            <!-- Add other form fields for editing -->
 
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary">Enregistrer les modifications</button>
-                        <a href="{{ route('destinations.index') }}" class="btn btn-secondary">Annuler</a>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Enregistrer les modifications</button>
+                                <a href="{{ route('destinations.index') }}" class="btn btn-secondary">Annuler</a>
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
--- resources/views/admin/vols/create.blade.php
+++ resources/views/admin/vols/create.blade.php
@@ -7,36 +7,42 @@
         </div>
         <div class="card-body">
             <form action="{{ route('vols.store') }}" method="POST">
-                @csrf
-                <div class="form-group">
-                    <label for="compagnie_aerienne">Compagnie Aérienne</label>
-                    <input type="text" name="compagnie_aerienne" class="form-control" id="compagnie_aerienne">
+                @csrf 
+                <div class="form_input" style="display:flex; justify-content:space-between;">
+                    <div class="form-group">
+                        <label for="compagnie_aerienne">Compagnie Aérienne</label>
+                        <input type="text" name="compagnie_aerienne" class="form-control" id="compagnie_aerienne" style="width: 300px;">
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="date_depart">Date de départ</label>
+                        <input type="datetime-local" name="date_depart" class="form-control" id="date_depart" style="width: 300px;">
+                    </div>
+                    <div class="form-group">
+                        <label for="prix">Prix</label>
+                        <input type="text" name="prix" class="form-control" id="prix" style="width: 300px;">
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="depart_id">Départ</label>
-                    <!-- Sélectionnez la destination de départ à partir d'une liste déroulante -->
-                    <select name="depart_id" class="form-control" id="depart_id">
-                        @foreach ($destinations as $destination)
-                            <option value="{{ $destination->id }}">{{ $destination->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="destination_id">Destination</label>
-                    <!-- Sélectionnez la destination de départ à partir d'une liste déroulante -->
-                    <select name="destination_id" class="form-control" id="destination_id">
-                        @foreach ($destinations as $destination)
-                            <option value="{{ $destination->id }}">{{ $destination->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="date_depart">Date de départ</label>
-                    <input type="datetime-local" name="date_depart" class="form-control" id="date_depart">
-                </div>
-                <div class="form-group">
-                    <label for="prix">Prix</label>
-                    <input type="text" name="prix" class="form-control" id="prix">
+                
+                <div style="display: flex; justify-content:space-between;">
+                    <div class="form-group">
+                        <label for="depart_id">Départ</label>
+                        <!-- Sélectionnez la destination de départ à partir d'une liste déroulante -->
+                        <select name="depart_id" class="form-control" id="depart_id" style="width: 480px;">
+                            @foreach ($destinations as $destination)
+                                <option value="{{ $destination->id }}">{{ $destination->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="destination_id">Destination</label>
+                        <!-- Sélectionnez la destination de départ à partir d'une liste déroulante -->
+                        <select name="destination_id" class="form-control" id="destination_id" style="width: 480px;">
+                            @foreach ($destinations as $destination)
+                                <option value="{{ $destination->id }}">{{ $destination->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
                 <!-- Ajoutez d'autres champs de formulaire pour les autres attributs du vol -->
                 <button type="submit" class="btn btn-primary">Enregistrer</button>
