009f8aac28a27bfba069b40ba198a70c2dea617d
yves51 <yvesarmand02@gmail.com>
2024-09-20 17:40:48 +0000

    dernière correction

--- resources/views/admin/reservations/show-vol.blade.php
+++ resources/views/admin/reservations/show-vol.blade.php
@@ -13,7 +13,6 @@
                 <div class="author-box-name">
                   <a href="#">{{$showvol->nom}} {{$showvol->prenoms}}</a>
                 </div>
-                <div class="author-box-job">Web Developer</div>
               </div>
               <div class="text-center">
                 <div class="author-box-description">
@@ -45,9 +44,7 @@
                     
                   </p>
                 </div>
-                <div class="mb-2 mt-3">
-                  <div class="text-small font-weight-bold">Follow Hasan On</div>
-                </div>
+                
                 <a href="#" class="btn btn-social-icon mr-1 btn-facebook">
                   <i class="fab fa-facebook-f"></i>
                 </a>
@@ -64,100 +61,24 @@
               </div>
             </div>
           </div>
-          {{-- <div class="card">
-            <div class="card-header">
-              <h4>Personal Details</h4>
-            </div>
-            <div class="card-body">
-              <div class="py-4">
-                <p class="clearfix">
-                  <span class="float-left">
-                    Birthday
-                  </span>
-                  <span class="float-right text-muted">
-                    30-05-1998
-                  </span>
-                </p>
-                <p class="clearfix">
-                  <span class="float-left">
-                    Phone
-                  </span>
-                  <span class="float-right text-muted">
-                    (0123)123456789
-                  </span>
-                </p>
-                <p class="clearfix">
-                  <span class="float-left">
-                    Mail
-                  </span>
-                  <span class="float-right text-muted">
-                    test@example.com
-                  </span>
-                </p>
-                <p class="clearfix">
-                  <span class="float-left">
-                    Facebook
-                  </span>
-                  <span class="float-right text-muted">
-                    <a href="#">John Deo</a>
-                  </span>
-                </p>
-                <p class="clearfix">
-                  <span class="float-left">
-                    Twitter
-                  </span>
-                  <span class="float-right text-muted">
-                    <a href="#">@johndeo</a>
-                  </span>
-                </p>
-              </div>
-            </div>
-          </div> --}}
-          {{-- <div class="card">
-            <div class="card-header">
-              <h4>Skills</h4>
-            </div>
-            <div class="card-body">
-              <ul class="list-unstyled user-progress list-unstyled-border list-unstyled-noborder">
-                <li class="media">
-                  <div class="media-body">
-                    <div class="media-title">Java</div>
-                  </div>
-                  <div class="media-progressbar p-t-10">
-                    <div class="progress" data-height="6">
-                      <div class="progress-bar bg-primary" data-width="70%"></div>
-                    </div>
-                  </div>
-                </li>
-                <li class="media">
-                  <div class="media-body">
-                    <div class="media-title">Web Design</div>
-                  </div>
-                  <div class="media-progressbar p-t-10">
-                    <div class="progress" data-height="6">
-                      <div class="progress-bar bg-warning" data-width="80%"></div>
-                    </div>
-                  </div>
-                </li>
-                <li class="media">
-                  <div class="media-body">
-                    <div class="media-title">Photoshop</div>
-                  </div>
-                  <div class="media-progressbar p-t-10">
-                    <div class="progress" data-height="6">
-                      <div class="progress-bar bg-green" data-width="48%"></div>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div> --}}
         </div>
         <div class="col-12 col-md-12 col-lg-8">
           <div class="card">
             <div class="padding-20">
-                <div class="card-header">
-                    <h4>Détails de réservation</h4>
+                  <div class="card-header d-flex justify-content-between">
+                      <h4>Détails de réservation</h4>
+                      <h4 class="ml-auto">Status de reservation :
+                          @if ($showvol->status === 'initialiser')
+                          <span class="badge badge-secondary">Initialisation</span>
+                          @elseif ($showvol->status === 'traiter')
+                          <span class="badge badge-primary">Traitement</span>
+                          @elseif ($showvol->status === 'valider')
+                          <span class="badge badge-success">Valider</span>
+                          @else
+                          <span class="badge badge-danger">Annuler</span>
+                          @endif
+                      </h4>
+
                   </div>
                   <div class="card-body">
                     <div class="py-4">
@@ -166,7 +87,7 @@
                                 Date de réservation:
                             </span>
                             <span class="float-right text-muted">
-                              <a href="#">{{$showvol->created_at}}</a>
+                             {{ ($showvol->created_at)->format('d/m/Y') }}
                             </span>
                           </p>
                       <p class="clearfix">
@@ -182,7 +103,7 @@
                             Destination d'Arrivée :
                         </span>
                         <span class="float-right text-muted">
-                          {{$showvol->destination_final}}
+                          {{($showvol->destination_final)}}
                         </span>
                       </p>
                       <p class="clearfix">
@@ -203,7 +124,7 @@
                       </p>
                       <p class="clearfix">
                         <span class="float-left">
-                            Nombre de passager:
+                            Nombre de passager(s):
                         </span>
                         <span class="float-right text-muted">
                           <a href="#">{{$showvol->num_persons}}</a>
@@ -211,7 +132,7 @@
                       </p>
                       <p class="clearfix">
                         <span class="float-left">
-                           Adulte:
+                           Adulte(s):
                         </span>
                         <span class="float-right text-muted">
                           <a href="#">{{$showvol->nombre_adultes}}</a>
@@ -219,7 +140,7 @@
                       </p>
                       <p class="clearfix">
                         <span class="float-left">
-                           Enfant de 2 à 12 ans:
+                           Enfant(s) de 2 à 12 ans:
                         </span>
                         <span class="float-right text-muted">
                           <a href="#">{{$showvol->nombre_enfant}}</a>
@@ -227,7 +148,7 @@
                       </p>
                       <p class="clearfix">
                         <span class="float-left">
-                           Bèbè de 0 à 2 ans:
+                           Bébé(s) de 0 à 2 ans:
                         </span>
                         <span class="float-right text-muted">
                           <a href="#">{{$showvol->nombre_bebe}}</a>
@@ -235,148 +156,6 @@
                       </p>
                     </div>
                   </div>
-             {{--  <ul class="nav nav-tabs" id="myTab2" role="tablist">
-                <li class="nav-item">
-                  <a class="nav-link active" id="home-tab2" data-toggle="tab" href="#about" role="tab"
-                    aria-selected="true">About</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" id="profile-tab2" data-toggle="tab" href="#settings" role="tab"
-                    aria-selected="false">Setting</a>
-                </li>
-              </ul>
-              <div class="tab-content tab-bordered" id="myTab3Content">
-                <div class="tab-pane fade show active" id="about" role="tabpanel" aria-labelledby="home-tab2">
-                  <div class="row">
-                    <div class="col-md-3 col-6 b-r">
-                      <strong>Full Name</strong>
-                      <br>
-                      <p class="text-muted">Emily Smith</p>
-                    </div>
-                    <div class="col-md-3 col-6 b-r">
-                      <strong>Mobile</strong>
-                      <br>
-                      <p class="text-muted">(123) 456 7890</p>
-                    </div>
-                    <div class="col-md-3 col-6 b-r">
-                      <strong>Email</strong>
-                      <br>
-                      <p class="text-muted">johndeo@example.com</p>
-                    </div>
-                    <div class="col-md-3 col-6">
-                      <strong>Location</strong>
-                      <br>
-                      <p class="text-muted">India</p>
-                    </div>
-                  </div>
-                  <p class="m-t-30">Completed my graduation in Arts from the well known and
-                    renowned institution
-                    of India – SARDAR PATEL ARTS COLLEGE, BARODA in 2000-01, which was
-                    affiliated
-                    to M.S. University. I ranker in University exams from the same
-                    university
-                    from 1996-01.</p>
-                  <p>Worked as Professor and Head of the department at Sarda Collage, Rajkot,
-                    Gujarat
-                    from 2003-2015 </p>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting
-                    industry. Lorem
-                    Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    when
-                    an unknown printer took a galley of type and scrambled it to make a
-                    type
-                    specimen book. It has survived not only five centuries, but also the
-                    leap
-                    into electronic typesetting, remaining essentially unchanged.</p>
-                  <div class="section-title">Education</div>
-                  <ul>
-                    <li>B.A.,Gujarat University, Ahmedabad,India.</li>
-                    <li>M.A.,Gujarat University, Ahmedabad, India.</li>
-                    <li>P.H.D., Shaurashtra University, Rajkot</li>
-                  </ul>
-                  <div class="section-title">Experience</div>
-                  <ul>
-                    <li>One year experience as Jr. Professor from April-2009 to march-2010
-                      at B.
-                      J. Arts College, Ahmedabad.</li>
-                    <li>Three year experience as Jr. Professor at V.S. Arts &amp; Commerse
-                      Collage
-                      from April - 2008 to April - 2011.</li>
-                    <li>Lorem Ipsum is simply dummy text of the printing and typesetting
-                      industry.
-                    </li>
-                    <li>Lorem Ipsum is simply dummy text of the printing and typesetting
-                      industry.
-                    </li>
-                    <li>Lorem Ipsum is simply dummy text of the printing and typesetting
-                      industry.
-                    </li>
-                    <li>Lorem Ipsum is simply dummy text of the printing and typesetting
-                      industry.
-                    </li>
-                  </ul>
-                </div>
-                <div class="tab-pane fade" id="settings" role="tabpanel" aria-labelledby="profile-tab2">
-                  <form method="post" class="needs-validation">
-                    <div class="card-header">
-                      <h4>Edit Profile</h4>
-                    </div>
-                    <div class="card-body">
-                      <div class="row">
-                        <div class="form-group col-md-6 col-12">
-                          <label>First Name</label>
-                          <input type="text" class="form-control" value="John">
-                          <div class="invalid-feedback">
-                            Please fill in the first name
-                          </div>
-                        </div>
-                        <div class="form-group col-md-6 col-12">
-                          <label>Last Name</label>
-                          <input type="text" class="form-control" value="Deo">
-                          <div class="invalid-feedback">
-                            Please fill in the last name
-                          </div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="form-group col-md-7 col-12">
-                          <label>Email</label>
-                          <input type="email" class="form-control" value="test@example.com">
-                          <div class="invalid-feedback">
-                            Please fill in the email
-                          </div>
-                        </div>
-                        <div class="form-group col-md-5 col-12">
-                          <label>Phone</label>
-                          <input type="tel" class="form-control" value="">
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="form-group col-12">
-                          <label>Bio</label>
-                          <textarea
-                            class="form-control summernote-simple">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur voluptatum alias molestias minus quod dignissimos.</textarea>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="form-group mb-0 col-12">
-                          <div class="custom-control custom-checkbox">
-                            <input type="checkbox" name="remember" class="custom-control-input" id="newsletter">
-                            <label class="custom-control-label" for="newsletter">Subscribe to newsletter</label>
-                            <div class="text-muted form-text">
-                              You will get new information about products, offers and promotions
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-footer text-right">
-                      <button class="btn btn-primary">Save Changes</button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>--}}
           </div>
         </div> 
       </div>
